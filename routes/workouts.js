3b63060be86b7a414cbb6be3da2bef9bc258ec96
yxs1n <yasin041121@gmail.com>
2025-12-11 03:57:27 +0000

    fix2

--- routes/workouts.js
+++ routes/workouts.js
@@ -11,7 +11,7 @@ function requireLogin(req, res, next) {
       type: 'error',
       message: 'You must be logged in to access that page.'
     };
-    return res.redirect('../users/login');
+    return res.redirect('./users/login');
   }
   next();
 }
